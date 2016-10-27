e4b3b22f6c3a3c72541f283d161c9062189b16ed
stanley7 <smt_xj@sina.com>
2016-10-27 15:31:42 +0800

    fasdf

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
@@ -20,11 +21,11 @@ public class Main {
         int exitTask=1;
         int min=T(task,1,1);
         int tasknumber=task.length;
-        for (int i=1;i<tasknumber-1;i++)
-            for (int j=i;i<=tasknumber-1;i++) {
+        for (int i=1;i<tasknumber-2;i++)
+            for (int j=i;j<tasknumber-1;j++) {
                 int totaltime=T(task,i,j);
                 if (totaltime < min) {
-                    entryTask = i;
+                entryTask = i;
                     exitTask = j;
                     min = totaltime;
                 }
@@ -32,9 +33,32 @@ public class Main {
         return Arrays.copyOfRange(task,entryTask,exitTask);
     }
 
-
+    public Task[] ceateTaskGraph(){
+        Task[] squentialTask=new Task[10];
+        for (int i=0;i<10;i++){
+            squentialTask[i]=new Task();
+            Random random=new Random();
+            squentialTask[i].taskid=i;
+            if (i!=9) {
+                squentialTask[i].childnumber=1;
+                squentialTask[i].child = new int[squentialTask[i].childnumber];
+                squentialTask[i].tnet = new int[squentialTask[i].childnumber];
+                squentialTask[i].child[0] = i + 1;
+                squentialTask[i].tnet[0]=random.nextInt(5)+1;
+            }
+            squentialTask[i].c=random.nextInt(10)+1;
+            squentialTask[i].m=random.nextInt(100)+1;
+            squentialTask[i].r=1;
+        }
+        return squentialTask;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main main=new Main();
+        Task[] task=main.ceateTaskGraph();
+        Task[] taskincloud=main.sequentailTask(task);
+        for (int i=0;i<taskincloud.length;i++){
+            System.out.print(taskincloud[i].taskid);
+        }
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -2,7 +2,12 @@
  * Created by jay on 2016/10/24.
  */
 public class Task {
+    /**
+     *
+     */
+    public int taskid=0;
     public int r=0;
+    public int childnumber;
     public int[] child;
     public int c;
     public int m;
